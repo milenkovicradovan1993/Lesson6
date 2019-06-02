3d1a548b4e64a43816c1d3350f10a1b8acb333b7
Radovan <Radovan1993@live.com>
2019-06-02 17:50:26 +0200

    BandNameGenerator change

--- src/exercises/BandNameGenerator.java
+++ src/exercises/BandNameGenerator.java
@@ -1,5 +1,11 @@
 package exercises;
 
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Random;
+import java.util.concurrent.ThreadLocalRandom;
+
 /* 
  * Make 2 ArrayLists; 
  *      1 full of adjectives (e.g. slimy, blind)
@@ -8,5 +14,27 @@ package exercises;
  * When the program is run, one word is randomly selected from each list to generate a band name.
 */
 public class BandNameGenerator {
+	
+	
+	public static void main(String[] args) {
+		List<String> adjectives = Arrays.asList("slimy","blind","white","black");
+		List<String> nouns = Arrays.asList("pants","bones","bones","wings");
+		 List<List<String>> totalList =  Arrays.asList(adjectives, nouns);
+		int adj = new Random().nextInt(adjectives.size());
+		int nou = new Random().nextInt(nouns.size());
+			System.out.print(adjectives.get(adj) + " " + nouns.get(nou));
+			}
+		
 
-}
+	
+		}
+	
+   
+
+		
+	
+
+
+
+
+
